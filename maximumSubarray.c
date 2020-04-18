1bc549d6e91f413647e2ee80ebfc60b5787210c9
Pattison Ho <yapi.tw@gmail.com>
2020-04-18 21:25:11 +0800

    Create maximumSubarray.c

---
+++ maximumSubarray.c
@@ -0,0 +1,22 @@
+int maxSubArray(int* nums, int numsSize){
+    int l = 0;
+    int r = 0;
+    int max = nums[0];
+    int maxEndHere = nums[0];
+    
+    for(int i=1; i < numsSize; i++) {
+        if(nums[i] > maxEndHere+nums[i]){
+            l = i;
+            maxEndHere = nums[i];
+        } else {
+            maxEndHere = maxEndHere + nums[i];
+        }
+        
+        if(max < maxEndHere) {
+            max = maxEndHere;
+            r = i;
+        }
+    }
+    
+    return max;
+}
