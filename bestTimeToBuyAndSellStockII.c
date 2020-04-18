e1aef30f1cd19d2a21dfe2cdc198948f27b87e11
Pattison Ho <yapi.tw@gmail.com>
2020-04-19 03:24:05 +0800

    Update bestTimeToBuyAndSellStockII.c

--- bestTimeToBuyAndSellStockII.c
+++ bestTimeToBuyAndSellStockII.c
@@ -11,6 +11,7 @@ int maxProfit(int* prices, int pricesSize){
 
 // Dynamic programming
 // Recursion
+// Will fail since LTE
 int maxProfit(int* prices, int pricesSize){
     // Boundary condition
     if (pricesSize <= 1) return 0;
@@ -33,4 +34,35 @@ int maxProfit(int* prices, int pricesSize){
         }
     }
     return max;   
+}
+
+// Simplize above solution
+int maxProfit(int* prices, int pricesSize){
+    if (pricesSize <= 1) return 0;
+    
+    int profits[pricesSize + 1];
+    profits[1] = 0;
+    
+    for(int k=2; k<=pricesSize; k++) {
+        // To sell or not to sell?
+        int profit;
+        int max = 0;
+
+        // 1) Not to sell
+        profit = profits[k-1];
+        if (profit > max) {
+            max = profit;
+        }
+
+        // 2) To sell
+        for (int i=1; i<=k-1; i++) {
+            profit = profits[i] + prices[k-1] - prices[i-1];
+            if (profit > max) {
+                max = profit;
+            }
+        }    
+        profits[k] = max;
+    }
+    
+    return profits[pricesSize];
 }
